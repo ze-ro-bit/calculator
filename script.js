9fb568080737fea51364f1bc58f5817b174b17b8
Mariyah Ysabelle <sofiyaaudrinnafranco5@gmail.com>
2023-03-13 08:05:35 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function display(value) {
+    document.getElementById('result').value += value;
+  }
+  
+  function clearResult() {
+    document.getElementById('result').value = '';
+  }
+  
+  function calculate() {
+    var result = eval(document.getElementById('result').value);
+    document.getElementById('result').value = result;
+  }
+  
